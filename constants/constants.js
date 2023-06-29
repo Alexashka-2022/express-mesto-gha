0c9a2b655366756ba97d0a0e07f1af170440202b
Alexashka-2022 <avibus.bonis.2022@gmail.com>
2023-06-29 12:52:39 +0300

    внесены изменения

--- constants/constants.js
+++ constants/constants.js
@@ -16,7 +16,8 @@ const allowedCors = ['http://shmakov.students.nomoreparties.sbs',
   'https://shmakov.students.nomoreparties.sbs',
   'http://api.shmakov.students.nomoreparties.sbs',
   'https://api.shmakov.students.nomoreparties.sbs',
-  'http://localhost:3000'];
+  'http://localhost:3000',
+  'http://localhost:3001'];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
